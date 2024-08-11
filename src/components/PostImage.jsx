1f4f1d037495989e402dd57afe71221e07ea024a
sammyalwayscode <olorundasamuel2@gmail.com>
2024-08-11 17:34:31 +0100

    Issue of Image Overriding resolved

--- src/components/PostImage.jsx
+++ src/components/PostImage.jsx
@@ -1,7 +1,12 @@
 import { addDoc, collection } from "firebase/firestore";
 import { useState } from "react";
 import { basedb, baseStorage } from "../Base";
-import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import {
+  getDownloadURL,
+  ref,
+  uploadBytes,
+  // uploadBytesResumable,
+} from "firebase/storage";
 import { useNavigate } from "react-router-dom";
 
 const PostImage = () => {
@@ -11,29 +16,51 @@ const PostImage = () => {
 
   const navigate = useNavigate();
 
-  const uploadImage = (e) => {
-    const file = e.target.files[0];
-    const fileRef = ref(baseStorage, "myImage" + file.name);
-    const storageRef = uploadBytesResumable(fileRef, file);
-    getDownloadURL(storageRef.snapshot.ref).then((url) => {
-      setAvatar(url);
-    });
-  };
+  // const uploadImage = (e) => {
+  //   const file = e.target.files[0];
+  //   const fileRef = ref(baseStorage, "myImage" + file.name);
+  //   const storageRef = uploadBytesResumable(fileRef, file);
+  //   getDownloadURL(storageRef.snapshot.ref).then((url) => {
+  //     setAvatar(url);
+  //   });
+  // };
 
   const postData = async () => {
+    // Step 1: Upload avatar to Firebase Storage
+    const storageRef = ref(baseStorage, `avatars/${avatar.name}`);
+    await uploadBytes(storageRef, avatar);
+
+    // Step 2: Get the download URL
+    const avatarURL = await getDownloadURL(storageRef);
+
     addDoc(collection(basedb, "techcampus"), {
       description,
       title,
-      avatar: await avatar,
+      avatar: avatarURL,
     });
     alert("Data posted sucessfully");
     navigate("/getimage");
     setTitle("");
   };
 
+  // const postData = async () => {
+  //   addDoc(collection(basedb, "techcampus"), {
+  //     description,
+  //     title,
+  //     avatar: await avatar,
+  //   });
+  //   alert("Data posted sucessfully");
+  //   navigate("/getimage");
+  //   setTitle("");
+  // };
+
   return (
     <div>
-      <input type="file" onChange={uploadImage} />
+      <input
+        type="file"
+        onChange={(e) => setAvatar(e.target.files[0])}
+        accept="image/*"
+      />
       <br />
       <input
         type="text"
